2b030d7e9b1cd65e0756efd85560eeb2303be704
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-03-22 22:36:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
 /* ================================
-   🧱 1. PROTEGER PAGINA
-================================ */
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-const supabase = createClient(
-  "https://qaophiaogsvhkgmbfcuf.supabase.co",
-  "sb_publishable_CAkCS2tdVxztSVlOt7tVNg_x2zuUTDN"
-);
-
-// 🔒 PROTEGER PÁGINA (VERSIÓN FINAL)
-export async function protegerPagina() {
-  const { data } = await supabase.auth.getSession();
-
-  if (!data.session) {
-    // ❌ No logueado → fuera
-    window.location.href = "/luna-blanca/index.html";
-  } else {
-    // ✅ Logueado → mostrar página
-    document.body.style.display = "block";
-  }
-}
-
-
-
-/* ================================
    🧱 1. CARGAR TARJETAS
 ================================ */
 document.getElementById("contenedor-tarjetas").innerHTML = tarjetasHTML;
