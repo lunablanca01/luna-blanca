47c0c279ebb98e2601238050137445e8de0b5efa
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-03-22 22:40:05 -0500

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,19 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const supabase = createClient(
+  "https://qaophiaogsvhkgmbfcuf.supabase.co",
+  "sb_publishable_CAkCS2tdVxztSVlOt7tVNg_x2zuUTDN"
+);
+
+// 🔒 SE EJECUTA AUTOMÁTICAMENTE
+(async () => {
+  document.body.style.display = "none";
+
+  const { data } = await supabase.auth.getSession();
+
+  if (!data.session) {
+    window.location.href = "/luna-blanca/index.html";
+  } else {
+    document.body.style.display = "block";
+  }
+})();
