d848e3a505966133b5aed77183881eb21c50d75d
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-03-24 22:25:42 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-export const supabase = createClient(
-  "https://qaophiaogsvhkgmbfcuf.supabase.co",
-  "sb_publishable_CAkCS2tdVxztSVlOt7tVNg_x2zuUTDN"
-);
+import { supabase } from "./supabase.js";
 
 // 🔒 SE EJECUTA AUTOMÁTICAMENTE
 (async () => {
