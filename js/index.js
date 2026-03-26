a5a02f04ed9488c2501060398d0e3181f5963c36
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-03-25 23:23:13 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ window.registrar = async () => {
   }
 
   if (password.length < 6) {
-    mensaje.innerText = "La contraseña debe tener al menos 6 caracteres 💭";
+    mensaje.innerText = "La contraseña debe tener al menos 6 caracteres";
     passwordInput.style.borderColor = "red";
     passwordInput.focus();
     return;
