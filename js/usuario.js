e609852f07db6d1f963845fd717580609ca5c697
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-03-31 18:38:03 -0500

    Update usuario.js

--- js/usuario.js
+++ js/usuario.js
@@ -1,4 +1,4 @@
-import { supabase } from "/supabase.js";
+import { supabase } from "./supabase.js";
 
 async function cargarUsuario() {
   const contenedor = document.getElementById("usuario-container");
