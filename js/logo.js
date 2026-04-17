c6c3ade785dc9865c2ecc8f809390a59fa54d568
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-04-16 20:37:07 -0500

    Add files via upload

---
+++ js/logo.js
@@ -0,0 +1,12 @@
+function cargarLogo() {
+  const contenedor = document.getElementById("logo-container");
+  if (!contenedor) return;
+
+  contenedor.innerHTML = `
+    <div class="logo-global">
+      <a href="/luna-blanca.html">Luna Blanca</a>
+    </div>
+  `;
+}
+
+cargarLogo();
