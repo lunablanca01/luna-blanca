b518d37206738e1f2afce670343fe3e6a71b5d1e
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-04-17 19:07:21 -0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-// 🔌 supabase.js - solo para remoto
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-export const supabase = createClient(
-  "https://qaophiaogsvhkgmbfcuf.supabase.co",
-  "sb_publishable_CAkCS2tdVxztSVlOt7tVNg_x2zuUTDN"
-);
