a3cdf1b65d21d73f254f4f1ec3240bdbeb95f40c
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-04-18 17:31:58 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -76,6 +76,14 @@ const cargarPendientes = async () => {
       </div>
     `;
   });
+
+  const { data, error } = await supabase
+  .from("perfiles")
+  .select("*")
+  .or("aprobado.eq.false,aprobado.is.null");
+
+console.log("PENDIENTES:", data);
+console.log("ERROR:", error);
 };
 
 
