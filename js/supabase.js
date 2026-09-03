ca88c6984d1cb801d3aaba8508f00cf3eed6c3f1
lunablanca01 <lunablanca.proyecto@gmail.com>
2026-09-02 20:43:43 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const supabase = createClient(
-  "https://qaophiaogsvhkgmbfcuf.supabase.co",
-  "sb_publishable_CAkCS2tdVxztSVlOt7tVNg_x2zuUTDN"
+  "https://kgzeuxpaneveotxaszbr.supabase.co",
+  "sb_publishable_Ywh63_8JKyMy6qqp6YYBrw_17q30Md9"
 );
